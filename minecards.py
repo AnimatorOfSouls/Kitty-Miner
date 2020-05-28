bdb45b6648af328e8c044d1b5e60d0c20c9c0ea0
AnimatorOfSouls <fantailfishy@gmail.com>
2020-05-28 16:18:07 +0100

    Changed structure to use bot.command

--- minecards.py
+++ minecards.py
@@ -1,4 +1,6 @@
 import discord
+from discord import Embed, Emoji
+from discord.ext.commands import Bot
 from discord.utils import get
 
 import os
@@ -7,9 +9,12 @@ from dotenv import load_dotenv
 
 import sqlite3
 import random
+#-----------------------------MAIN------------------------------#
+bot = Bot(command_prefix=".mc ")
 
 
-#only activate this if new table added or old database deleted
+
+#For bot owner use only!!! Make fresh database if not already existing
 def make_db():
     #Connecting to database
     conn = sqlite3.connect("MineCards.db")
@@ -23,27 +28,88 @@ def make_db():
 
 
 
-def claim_card():
+#Allows user to claim a card
+@bot.command(pass_context=True)
+async def claim(ctx):
+    #Rarotoes = Common, Uncommon, Rare,
+    rarities = ["white","blue","green","orange","red","pink","purple"]
+    weight = [0.5,0.3,0.1,0.05,0.03,0.01,0.005]
+    test = random.choices(rarities, weight, k=1)
+    print(test)
+
+    #Choosing the item + tier
     rand_card = random.randint(1,2)
-    common = random.randint(2,5)
+
+    #Number of the item to give (more items for more common rarities)
+    common = random.randint(5,10)
+    uncommon = random.randint(3,7)
+
     if rand_card == 1:
-        card = "+" + str(common) + " Stone"
+        type = "stone"
+        rarity = "c"
     elif rand_card == 2:
-        card = "+" + str(common) + " Wood"
-    else:
-        print("CLAIM FAILED! BEEP BOOP BUG FOUND!")
-
-    return card
+        type = "wood"
+        rarity = "c"
 
 
 
-def new_profile(user):
+    #Connecting to database and creating a cursor to navigate the database
+    conn = sqlite3.connect("MineCards.db")
+    cursor = conn.cursor()
+
+    #Finding the amount of cards in the player's inventory
+    print(type)
+    cursor.execute("SELECT "+type+" FROM Profiles WHERE Username = '" + str(ctx.message.author) + "'")
+    result = cursor.fetchall()
+
+    amount = result[0][0]
+
+    if rarity == "c":
+        new_amount = amount + common
+        card = str(common) + "x " + type
+    elif rarity == "u":
+        new_amount = amount + uncommon
+        card = str(uncommon) + "x " + type
+
+
+    #Updating values
+    sql = "UPDATE Profiles SET " + type + " = " + str(new_amount) + " WHERE username = '" + str(ctx.message.author) + "'"
+
+    cursor.execute(sql)     #Updating
+    conn.commit()           #Saving the entry
+
+
+    #Closing the connections
+    cursor.close()
+    conn.close()
+
+
+
+    embed = discord.Embed(title="You picked up a new card!", color=0x00fff0)
+    embed.add_field(name="Card:", value=card, inline=False)
+    embed.add_field(name="Total:", value=new_amount, inline=False)
+    await ctx.message.channel.send(embed=embed)
+
+
+
+#Displays different tiers of cards in order of most common to least common
+@bot.command(pass_context=True)
+async def tiers(ctx):
+    embed = discord.Embed(title="Card Tiers:", color=0x00fff0)
+    embed.add_field(name="Most Common -> Least Common", value="<:white:715571455370199101> White\n<:blue:715573192185610341> Blue\n<:green:715573216130629682> Green\n<:orange:715573223965851668> Orange\n<:red:715573238004056084> Red\n<:pink:715573244916400279> Pink\n<:purple:715573253808062555> Purple", inline=False)
+    await ctx.message.channel.send(embed=embed)
+
+
+
+#Makes a new profile for a new user
+@bot.command(pass_context=True)
+async def newprofile(ctx):
     #Connecting to database and creating a cursor to navigate the database
     conn = sqlite3.connect("MineCards.db")
     cursor = conn.cursor()
 
     #Check if username is already in the Profiles table
-    cursor.execute("SELECT Username FROM Profiles WHERE Username = '" + user + "'")
+    cursor.execute("SELECT Username FROM Profiles WHERE Username = '" + str(ctx.message.author) + "'")
 
     result = cursor.fetchall()
 
@@ -69,7 +135,7 @@ def new_profile(user):
         sql = "INSERT INTO Profiles (username,stone,wood) VALUES (?,?,?)"
 
 
-        values = [user,0,0]
+        values = [str(ctx.message.author),0,0]
 
         #Connecting to database and creating a cursor to navigate the database
         conn = sqlite3.connect("MineCards.db")
@@ -82,59 +148,65 @@ def new_profile(user):
         cursor.close()
         conn.close()
 
-        return found
+        await ctx.message.channel.send("New profile created. To view your profile, do .minecards profile")
+
 
     #Do nothing if username was found
     else:
-        return found
+        await ctx.message.channel.send("You already have a profile!")
 
 
-### --------------------- MAIN --------------------- ###
-client = discord.Client()
 
+#Shows the user's inventory
+@bot.command(pass_context=True)
+async def inventory(ctx):
+    #Connecting to database and creating a cursor to navigate the database
+    conn = sqlite3.connect("MineCards.db")
+    cursor = conn.cursor()
+
+    #Check if username is already in the Profiles table
+    cursor.execute("SELECT wood,stone FROM Profiles WHERE username = '" + str(ctx.message.author) + "'")
+
+    result = cursor.fetchall()
+
+    inv = []
+
+    for row in result:
+        inv.append(row)
+
+    #Closing the connections
+    cursor.close()
+    conn.close()
+
+
+    #Creating embed
+    wood = inv[0][0]
+    stone = inv[0][1]
+
+    embed = discord.Embed(title="Inventory:", color=0x7C8ED0)
+    embed.add_field(name="Username:", value=ctx.message.author, inline=False)
+    embed.add_field(name="Wood:", value=wood, inline=False)
+    embed.add_field(name="Stone:", value=stone, inline=False)
+
+
+    await ctx.message.channel.send(embed=embed)
+
+
+
+#Shows the user's profile
+@bot.command(pass_context=True)
+async def profile(ctx):
+    embed = discord.Embed(title="Profile:", color=0x7C8ED0)
+    embed.set_thumbnail(url=ctx.message.author.avatar_url)
+    embed.add_field(name="Username:", value=ctx.message.author, inline=False)
+
+    await ctx.message.channel.send(embed=embed)
+
+
+
+
+#-------------------------TOKEN---------------------------------#
 load_dotenv()
 token = os.getenv("TOKEN")
 
-@client.event
-async def on_message(message):
-    message.content = message.content.lower()
-
-    #Prevents bot from seeing own messages
-    if message.author == client.user:
-        return
-
-    ### Main ###
-    if message.content.startswith(".minecards") or message.content.startswith(".mc"):
-        if "claim" in message.content:
-            card = claim_card()
-
-            embed = discord.Embed(title="You picked up a new card!", color=0x00fff0)
-            embed.add_field(name="Card:", value=card, inline=False)
-            await message.channel.send(embed=embed)
-
-
-
-        #Allow user to view their profile
-        elif "profile" in message.content:
-            await message.channel.send("PROFILE PLACEHOLDER")
-
-
-        #Make a new profile for user
-        elif "new" in message.content:
-            user = str(message.author)
-            existing = new_profile(user)
-
-            if existing == True:
-                await message.channel.send("You already have a profile!")
-            else:
-                await message.channel.send("New profile created. To view your profile, do .minecards profile")
-
-
-        #Invalid command
-        else:
-            await message.channel.send("Invalid command, to see a list of commands do .minecards help")
-
-        #if str(message.author) == "AnimatorOfSouls#2980":
-
-
-client.run(token)
+bot.run(token)
